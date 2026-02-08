453f7dec39b6d4a6766e61a37329ebfdd682747a
Toganner <124211833+Toganner@users.noreply.github.com>
2026-02-07 21:54:29 -0600

    second iteration of arduino
    takes two values of speed (target and current) as an integer in seconds, compares your speed to the target to give a percent, and displays this information on the screen. The screen will tell you your target pace in yellow, a big "SLW DWN" / "SPD UP" / "ON PACE" in blue text depending on the calculation, and your current pace in small blue text.

--- Pace_Giver.ino
+++ Pace_Giver.ino
@@ -1,19 +1,33 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <math.h>
+#include <Arduino.h>
+
+/*
+From program to watch
+Pace (in seconds)
+Activity: run, hike
+
+From phone to watch
+GPS coordinates
+*/
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
 
-#define WALKING 1
-#define RUNNING 2
-#define HIKING 3
+#define RUNNING 1
+#define HIKING 2
 
-int PACE_TOLERANCE;
-int CURRENT_PACE;
-int TARGET_PACE;
+#define SPEED_UP 1
+#define SLOW_DOWN 2
+#define ON_PACE 3
+
+int PACE_TOLERANCE = 10; // in percent
+int CURRENT_PACE; // in seconds
+int TARGET_PACE; // in seconds
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -33,20 +47,65 @@ void setup() {
   drawScreen("Target: ", "BOOTING", "Current: ");
 }
 
+/////////////LOOP/////////////
 void loop() {
-  delay(1000);
-  drawScreen("Target: 7:00", "SPD UP", "Current: 8:00");
-  delay(1000);
-  drawScreen("Target: 8:45", "ON PACE", "Current: 8:47");
-  delay(1000);
-  drawScreen("Target: 8:45", "SLW DWN", "Current: 8:47");
+  slowDownTest();
+  delay(2000);
+  onPaceTest();
+  delay(2000);
+  speedUpTest();
+  delay(2000);
+}
+/////////////LOOP/////////////
+
+// ---------- Helpers ----------
+static inline double deg2rad(double deg) {
+  return deg * (M_PI / 180.0);
+}
+static inline double mpsToKph(double mps) { 
+  return mps * 3.6; 
+}
+static inline double mpsToMph(double mps) { 
+  return mps * 2.2369362920544; 
+}
+int percentFromTarget() {
+  // negative percent means you're going faster so slow down
+  // positive percent means you're going slower so speed up
+  if (TARGET_PACE == 0) { return 0; }
+  return (CURRENT_PACE - TARGET_PACE) * 100 / TARGET_PACE;
+}
+String intSecondsToStringMinutes(int seconds) {
+  int minutes = seconds / 60;
+  int leftoverSeconds = seconds % 60;
+  String str = String(minutes) + ":";
+  if (leftoverSeconds < 10) { str += "0"; }
+  str += String(leftoverSeconds);
+  return str;
+}
+// ---------- End Helpers ----------
+
+
+// Chooses what to set the screen based on pace metrics
+void setScreen(int percent) {
+  String curPace = "Current: " + intSecondsToStringMinutes(CURRENT_PACE);
+  String tarPace = "Target: " + intSecondsToStringMinutes(TARGET_PACE);
+  // use ".c_str()" to convert from String to "const char*"
+
+  if (abs(percent) < PACE_TOLERANCE) {
+    drawScreen(tarPace.c_str(), "ON PACE", curPace.c_str());
+  }
+  else if (percent > PACE_TOLERANCE) {
+    drawScreen(tarPace.c_str(), "SPD UP", curPace.c_str());
+  }
+  else if (percent < -PACE_TOLERANCE) {
+    drawScreen(tarPace.c_str(), "SLW DWN", curPace.c_str());
+  }
+  else { // failsafe
+    drawScreen("ERROR", "ERROR", "ERROR");
+  }
 }
 
-int setScreen() {
-
-}
-
-/* -------- Screen Drawing -------- */
+// Draws three lines of text on the screen
 void drawScreen(const char* top,
                 const char* middle,
                 const char* bottom) {
@@ -78,4 +137,34 @@ void drawScreen(const char* top,
   display.println(bottom);
 
   display.display();
+}
+
+void speedUpTest(){
+  int current = 9*60; // 9 min
+  int target = 8*60; // 8 min
+
+  CURRENT_PACE = current;
+  TARGET_PACE = target;
+
+  setScreen(percentFromTarget());
+}
+
+void onPaceTest(){
+  int current = 9*60; // 9 min
+  int target = 9*60; // 9 min
+
+  CURRENT_PACE = current;
+  TARGET_PACE = target;
+
+  setScreen(percentFromTarget());
+}
+
+void slowDownTest(){
+  int current = 8*60; // 9 min
+  int target = 9*60; // 9 min
+
+  CURRENT_PACE = current;
+  TARGET_PACE = target;
+
+  setScreen(percentFromTarget());
 }
