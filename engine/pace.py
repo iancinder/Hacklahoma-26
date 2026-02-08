b64f4da72d4cb3c02f04089328d59d778601cf66
iancinder <iansendelbach@gmail.com>
2026-02-07 22:10:26 -0600

    changed pace.py to include fitness level in calculations

--- engine/pace.py
+++ engine/pace.py
@@ -2,7 +2,7 @@
 Pace Calculation
 '''
 
-def estimate_time(distance_mi, elevation_gain_ft, flat_speed_mph, vertical_speed_fph, difficulty):
+def estimate_time(distance_mi, elevation_gain_ft, flat_speed_mph, vertical_speed_fph, difficulty, fitness_level):
     # Base time
     flat_time = distance_mi / flat_speed_mph                # [hours]
     climb_time = elevation_gain_ft / vertical_speed_fph     # [hours]
@@ -10,6 +10,11 @@ def estimate_time(distance_mi, elevation_gain_ft, flat_speed_mph, vertical_speed
     # difficulty multiplier
     difficulty_factor = {"easy": 1.0, "moderate": 1.15, "hard": 1.3}
 
-    total_time = (flat_time + climb_time) * difficulty_factor.get(difficulty, 1.0)
+    # fitness level
+    fitness_modifier = 1.2 - (0.3 * fitness_level) # 0.9 to 1.2 multiplier based on fitness level
+
+    total_time = (flat_time + climb_time) * difficulty_factor.get(difficulty, 1.0) * fitness_modifier
+
+    
 
     return total_time
